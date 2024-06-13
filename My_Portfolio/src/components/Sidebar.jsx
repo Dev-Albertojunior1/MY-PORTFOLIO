2c2c96284bbc3b8da64b08e73a90823f80654364
studentSystem <albertojunior848423803@gmail.com>
2024-06-12 20:28:33 -0700

    first commit

---
+++ My_Portfolio/src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+import SocialNetworks from "./SocialNetworks";
+import Avatar from "../img/me.jpeg";
+import '..//styles/components/sidebar.sass';
+
+
+const Sidebar = () => {
+  return (
+   <aside id="sidebar">
+
+    <img src={Avatar} alt="Alberto Junior" />
+    <p className="title"> Developer</p>
+    <SocialNetworks/>
+    <p>informacoes de contacto</p>
+    <a href="" className="btn">
+        Download Curriculum
+    </a>
+   </aside>
+  )
+}
+
+export default Sidebar
